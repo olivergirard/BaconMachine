35867982bb23a1d64491385186f35fb30ebc2d5d
oliver.girard <girard1@purdue.edu>
2022-02-20 18:35:58 -0500

    blocked off certain combinations

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 
 void english_to_cipher(char *s, char *sentence, char* cipher_sentence);
 int MAX_LENGTH = 500;
+int BAD_I_SIZE = 14;
+int BAD_U_SIZE = 3;
 
 int main() {
 
@@ -40,6 +42,8 @@ void english_to_cipher(char *s, char *sentence, char *cipher_sentence) {
   int size = 0;
   char *cipher_array[26] = {"aaaaa", "aaaab", "aaaba", "aaabb", "aabaa", "aabab", "aabba", "aabbb", "abaaa", "abaaa", "abaab", "baaaa", "ababb", "abbaa", "abbab", "abbba", "abaab", "baaaa", "baaab", "baaba", "baabb", "aabab", "babaa", "abaab", "babba", "babbb"};
   char *language_array[26] = { "a", "by", "ch", "d", "e", "f", "gy", "hy", "i", "j", "ky", "r", "my", "ny", "o", "py", "k", "ry", "sh", "t", "u", "f", "w", "s", "y", "z" };
+  char *bad_i[] = {"by", "d", "gy", "hy", "ky", "my", "ny", "py", "ry", "s", "t", "w", "y", "z"};
+  char *bad_u[] = {"d", "t", "w"};
 
   //size of s
   while (s[size] != '\0') {
@@ -85,38 +89,66 @@ void english_to_cipher(char *s, char *sentence, char *cipher_sentence) {
 
   printf("Encoded sentence: %s\n", cipher_sentence);
   bool consonant = true;
+  char *previous_consonant = "\0";
+  previous_consonant = (char*) malloc((MAX_LENGTH * sizeof(char)));
+  bool bad_consonant = false;
 
   //bacon time! a space should come after every vowel. print to terminal
   for (int p = 0; p < size; p++) {
     if (cipher_sentence[p] == 'a') {
+    //this makes a consonant
       if (consonant == true) {
-        do {
-          random = (rand() % 13);
-        } while ((random != 0) && (random != 4) && (random != 8));
-        consonant = false;
-      } else {
         do {
           random = (rand() % 13);
         } while ((random == 0) || (random == 4) || (random == 8));
+        consonant = false;
+        previous_consonant = language_array[random];
+        } else {
+        //this makes a or e or i
+        do {
+          random = (rand() % 13);
+        } while ((random != 0) && (random != 4) && (random != 8));
         consonant = true;
       }
     } else {
+    //this makes a consonant
       if (consonant == true) {
         do {
           random = (rand() % 13) + 13;
-        } while ((random != 14) && (random != 20));
+        } while ((random == 14) || (random == 20));
         consonant = false;
+        previous_consonant = language_array[random];
       } else {
+      //this makes o or u
         do {
           random = (rand() % 13) + 13;
-        } while ((random == 14) || (random == 20));
+        } while ((random != 14) && (random != 20));
         consonant = true;
       }
     }
-    printf("%s", language_array[random]);
-    if ((random == 0) || (random == 4) || (random == 8) || (random == 14) || (random == 20)) {
-      printf(" ");
+
+    for (int i = 0; i < BAD_I_SIZE; i++) {
+      if ((strcmp(previous_consonant, bad_i[i]) == 0) && (strcmp(language_array[random], "i") == 0)) {
+        bad_consonant = true;
+      }
     }
+
+    for (int i = 0; i < BAD_U_SIZE; i++) {
+      if ((strcmp(previous_consonant, bad_u[i]) == 0) && (strcmp(language_array[random], "u") == 0)) {
+        bad_consonant = true;
+      }
+    }
+
+    if (bad_consonant == true) {
+      p--;
+    } else {
+      printf("%s", language_array[random]);
+      if ((random == 0) || (random == 4) || (random == 8) || (random == 14) || (random == 20)) {
+        printf(" ");
+      }
+    }
+
+    random = 0;
   }
   printf("\n");
 }
